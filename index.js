095a1b721a8155e56535b61d5904d8812dfa99bb
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-10-06 12:12:47 +0900

    Update AddTask

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import { App } from "./src/App.js"
 const app = new App();
-　
+app.mount();
+
+
+const formElement = document.querySelector("#js-form");
+
+formElement.addEventListener("submit",(event)=>{
+
+})
+
+const inputElement = document.querySelector("#js-form-input");
+console.log(inputElement.value);
